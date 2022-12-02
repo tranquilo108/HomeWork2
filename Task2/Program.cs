102370a8e81355aa610cdbbf1581078693c42755
tranquilo108 <sanbonzakurakogyoshi@gmail.com>
2022-12-03 02:18:03 +0700

    Initial commit

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (Math.Abs(number) < 100)
+    Console.WriteLine("Третьей цифры нет!");
+else
+{
+    string x = Convert.ToString(number);
+    Console.WriteLine("Третья цифра вашего числа: " + x[2]);
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+static void Main(string[] args)
+{
+    Console.Write("Введите число искомого дня недели: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number == 7 || number == 6)
+    {
+        Console.WriteLine("ДА");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ");
+    }
+}
